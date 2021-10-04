e3f8869d69aa34b378c9b77d8282b1c9e8cb8c9b
space900 <beyondmyspace@yandex.ru>
2021-10-04 12:37:28 +0300

    added 'foo_, _bar' for .eslintrc, added scripts start, dev(nodemon) for app.js

--- app.js
+++ app.js
@@ -5,5 +5,10 @@ const app = express();
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
+  // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send(req.query);
+}); 
