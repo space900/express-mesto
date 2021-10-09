f4e120bb5d41b0332f78dff240c5b1167ffbec70
space900 <beyondmyspace@yandex.ru>
2021-10-09 20:44:19 +0300

    fixed messages.js(created object with error message), deleted app.post from app.js, app.use(_id) added high priority

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 // eslint-disable-next-line no-unused-vars
-const path = require('path');
-const { createUser } = require('./controllers/users');
 const usersRoutes = require('./routes/users');
 const cardsRoutes = require('./routes/cards');
 
@@ -14,16 +12,16 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 app.use(express.json());
-app.post('/', createUser);
 
-app.use('/', usersRoutes);
-app.use('/', cardsRoutes);
 app.use((req, res, next) => {
   req.user = {
     _id: '615b330494e76a795d48c9b3',
   };
   next();
 });
+
+app.use('/', usersRoutes);
+app.use('/', cardsRoutes);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
--- errors/messages.js
+++ errors/messages.js
@@ -1,10 +1,10 @@
 module.exports = {
-  BAD_REQUEST_USER_SEARCH: 'Запрашиваемый пользователь не найден',
-  BAD_REQUEST_USER_CREATE: 'Переданы некорректные данные при создании пользователя',
-  BAD_REQUEST_USER_UPD: 'Переданы некорректные данные при обновлении профиля',
-  BAD_REQUEST_AVATAR_UPD: 'Переданы некорректные данные при обновлении аватара',
-  BAD_REQUEST_CARD: 'Переданы некорректные данные',
-  NOT_FOUND: 'Пользователь по указанному _id не найден',
-  NOT_FOUND_CARD: 'Карточка с указанным _id не найдена',
-  INTERNAL_SERVER: 'Произошла ошибка на сервере',
+  BAD_REQUEST_USER_SEARCH: { message: 'Запрашиваемый пользователь не найден' },
+  BAD_REQUEST_USER_CREATE: { message: 'Переданы некорректные данные при создании пользователя' },
+  BAD_REQUEST_USER_UPD: { message: 'Переданы некорректные данные при обновлении профиля' },
+  BAD_REQUEST_AVATAR_UPD: { message: 'Переданы некорректные данные при обновлении аватара' },
+  BAD_REQUEST_CARD: { message: 'Переданы некорректные данные' },
+  NOT_FOUND: { message: 'Пользователь по указанному _id не найден' },
+  NOT_FOUND_CARD: { message: 'Карточка с указанным _id не найдена' },
+  INTERNAL_SERVER: { message: 'Произошла ошибка на сервере' },
 };
