ef9e869b9ec4c812ac9c9e652f0ab0ef84919f57
space900 <beyondmyspace@yandex.ru>
2021-10-28 14:53:40 +0300

    installed winston, created and configured logger.js for middlewares

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const NotFound = require('./errors/classes/notFound');
 const messages = require('./errors/messages');
 const { auth } = require('./middlewares/auth');
 const { loginValidation, userValidation } = require('./middlewares/validation');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -24,6 +25,8 @@ const limiter = rateLimit({
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
+
+app.use(requestLogger);
 
 app.use(cookieParser());
 app.use(helmet());
@@ -39,6 +42,8 @@ app.use('/', auth, cardsRoutes);
 app.use('*', () => {
   throw new NotFound(messages.SERVER_NOT_FOUND);
 });
+
+app.use(errorLogger);
 app.use(errors());
 
 app.use((err, req, res, next) => {
